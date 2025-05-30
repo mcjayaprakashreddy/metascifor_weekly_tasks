cc5a83d7ce51f80c4b5a8346a2c45e07a98528b2
Jayaprakash Reddy Mula Chennaiah Gari <mcjayaprakashreddy8309@gmail.com>
2025-05-30 19:34:33 +0530

    Add files via upload

---
+++ Task8/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+import Hello from './components/Function'
+import HelloComponent from './components/weektask6/HelloComponent'
+import Welcome from './components/weektask6/Welcome'
+import Toggle from './components/Weektask8/Toggle'
+
+
+function App() {
+  
+
+  return (
+    <>
+     {/* <Hello/> */}
+     {/* <Welcome name="Jayaprakash"/>
+     <HelloComponent/>
+     <Counter/> */}
+   <Toggle/>
+    </>
+  )
+}
+
+export default App
